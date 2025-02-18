bfacf2a0b15dd9854a65b87417881e7fba565815
Nurul Fauzi <cunduck99@gmail.com>
2025-02-19 02:47:59 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,15 +10,15 @@ from modules.prompt_generator import generate_image_prompt
 from modules.image_generation import generate_image
 from modules.interactive_visualization import create_gif
 
-# Load environment variables
+
 load_dotenv()
 
-# Sidebar untuk upload dokumen
+
 st.sidebar.title("Smart Document Assistant 🧠")
 uploaded_file = st.sidebar.file_uploader("Upload (PDF/Word)", type=["pdf", "docx"])
 
-# Generate random data untuk statistik
-np.random.seed(None)  # Reset seed agar hasil random berubah setiap reload
+
+np.random.seed(None)  
 llm_stats = {
     "Average Response Time (s)": round(np.random.uniform(1.5, 3.5), 2),
     "Accuracy (%)": round(np.random.uniform(85, 99), 2),
@@ -32,33 +32,33 @@ image_stats = {
 
 
 
-# Tombol untuk memulai proses
+
 if st.sidebar.button("start"):
     if uploaded_file:
-        # Load token dari .env
+       
         hf_token_llm = os.getenv("HF_TOKEN_LLM")
         hf_token_image = os.getenv("HF_TOKEN_IMAGE")
         
         if not hf_token_llm or not hf_token_image:
             st.error("Silakan masukkan token di file .env.")
         else:
-            # Load dokumen
+          
             raw_text = load_document(uploaded_file)
             clean_text = preprocess_text(raw_text)
             
-            # Summarization
+          
             summary = summarize_text(clean_text, hf_token_llm)
             
-            # Generate image prompt
+            
             image_prompt = generate_image_prompt(summary, hf_token_llm)
             
-            # Generate image
+           
             image = generate_image(image_prompt, hf_token_image)
             
-            # Create GIF
+          
             create_gif(["outputs/mindmap.png"], "outputs/visualization.gif")
             
-            # Halaman utama
+          
             st.title("Smart Document Assistant 🧠")
             col1, col2 = st.columns(2)
             
@@ -70,4 +70,4 @@ if st.sidebar.button("start"):
                 st.subheader("Visualiszation")
                 st.image(image, caption="Mind Map", use_container_width=True)
     else:
-        st.error("Silakan unggah dokumen terlebih dahulu.")
+        st.error("Please upload your document first.")
